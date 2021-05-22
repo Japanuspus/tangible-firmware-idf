4b127f24b41a1a60cf89ac709392bc484ee43d0b
Janus <janus@insignificancegalore.net>
2021-05-22 13:49:12 +0200

    Init NVS to store radio calibration
    Without this initialization, I got warnings about radios recalibrating on each run.

--- components/network/network.c
+++ components/network/network.c
@@ -7,7 +7,7 @@
 #include "esp_http_client.h"
 #include "esp_event.h"
 #include "esp_log.h"
-// #include "nvs_flash.h"
+#include "nvs_flash.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
@@ -213,15 +213,12 @@ void wifi_init_sta(void)
 void network_init(void)
 {
     //Initialize NVS
-    /*
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
       ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
-    */
-
     wifi_init_sta();
 }
 
